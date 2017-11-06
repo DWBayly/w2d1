7a2cc6522704e0b29b3f88ef5949e7d3cd145fa8
dbaylyuvic <dbayly@uvic.ca>
2017-11-06 09:29:54 -0800

    added get and print html

--- httpclient2.js
+++ httpclient2.js
@@ -1,28 +1,31 @@
 var https=require('https');
-function getAndPrintHTMLChunks () {
-var requestOptions = {
+var sumdata= "";
+function getAndPrintHTML () {
+
+  var requestOptions = {
     host: 'sytantris.github.io',
-    path: '/http-examples/step1.html'
+    path: '/http-examples/step2.html'
   };
-function
+
+
  https.get(requestOptions, function (response) {
 
   // set encoding of received data to UTF-8
   response.setEncoding('utf8');
 
+
   // the callback is invoked when a `data` chunk is received
   response.on('data', function (data) {
-    console.log("\n"+ data+"\n");
+    console.log("Data recieved");
+    sumdata+= data;
+    //console.log(sumdata);
   });
-
+  response.on('end',function(){
+    console.log(sumdata);
+  });
   // the callback is invoked when all of the data has been received
   // (the `end` of the stream)
-  response.on('end', function() {
-    console.log('Response stream complete.');
-  });
 
 });
-
 }
-getAndPrintHTMLChunks();
-
+getAndPrintHTML();
